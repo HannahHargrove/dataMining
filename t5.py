33f352ef933f9d6ab9504824a614d9c93bc4fb9c
HannahHargrove <34074913+HannahHargrove@users.noreply.github.com>
2018-09-22 17:54:49 -0500

    Create t5.py

---
+++ t5.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import *
+from sklearn import tree, preprocessing
+
+from sklearn.externals.six import StringIO
+import pydot
+import pprint as pp
+
+# Read in the csv of Q3 training data
+q3train = pd.read_csv('q3_train.csv', sep= ',')
+
+# set the categorical data to binary values 0 and 1
+train1 = pd.get_dummies(q3train.loc[:, 'location':'media'])
+train2 = pd.get_dummies(q3train.label)
+
+# Read in the csv of Q3 test data
+data = pd.read_csv('q3_test.csv', sep=',')
+
+# set the test categorical data to binary values
+test1 = pd.get_dummies(data.loc[:, 'location':'media'])
+
+test2 = data.label
+
+# set criterion to gini for C4.5 and entropy for ID3
+gini_entropy = DecisionTreeClassifier(criterion='gini', random_state=100, max_depth=8, min_samples_leaf=4)
+#gini_entropy = DecisionTreeClassifier(criterion='entropy', random_state=100, max_depth=8, min_samples_leaf=4)
+gini_entropy.fit(train1, train2)
+
+# make the decision tree
+predict2 = gini_entropy.predict(test1)
+le = preprocessing.LabelEncoder()
+le.fit(test2)
+test2 = le.transform(test2)
+print(type(test2))
+predict2 = predict2[:, 1]
